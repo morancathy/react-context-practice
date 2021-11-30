f146111d8d7578948336786f8d774fbb17948c28
Cat <morancathy@gmail.com>
2021-11-30 11:41:34 -0600

    first commit with basic file layout

---
+++ src/components/Booklist.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Booklist extends React.Component {
+  render() {
+    return (
+      <div className="book-list">
+        <ul>
+          <li>Green Eggs and Ham</li>
+          <li>Oh the Places you can go</li>
+          <li>A Beauitful Place to Die</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Booklist;
