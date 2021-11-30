81ec0a6add1ed4563b2098b9a82bbff89b321b8e
Cat <morancathy@gmail.com>
2021-11-30 12:12:30 -0600

    accessing context using static property

--- src/components/Booklist.js
+++ src/components/Booklist.js
@@ -1,13 +1,20 @@
 import React, { Component } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
 
 class Booklist extends React.Component {
+  static contextType = ThemeContext;
   render() {
+    const { isLightTheme, light, dark } = this.context;
+    const theme = isLightTheme ? light : dark;
     return (
-      <div className="book-list">
+      <div
+        className="book-list"
+        style={{ color: theme.syntax, background: theme.bg }}
+      >
         <ul>
-          <li>Green Eggs and Ham</li>
-          <li>Oh the Places you can go</li>
-          <li>A Beauitful Place to Die</li>
+          <li style={{ background: theme.ui }}>Green Eggs and Ham</li>
+          <li style={{ background: theme.ui }}>Oh the Places you can go</li>
+          <li style={{ background: theme.ui }}>A Beauitful Place to Die</li>
         </ul>
       </div>
     );
